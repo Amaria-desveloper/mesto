e9cb45122d8314995f8728d9e2e3a109223ee1aa
Maria Aleksandrova <mail@aleksandrovamaria.ru>
2021-04-24 16:52:15 +0300

    fix: упростить сценарий

--- scripts/script.js
+++ scripts/script.js
@@ -11,68 +11,34 @@ let inputProfileName = formEditProfile.querySelector(`.popup__input[name="profil
 let inputProfileDesc = formEditProfile.querySelector(`.popup__input[name="profileDescription"]`);
 
 
-/**
- * Открывает попап
- * @returns {void}
- */
-function openPopup() {
+// Закрывает попап
+function openPopup(evt) {
+  evt.preventDefault();
+
   popup.classList.add(`popup_opened`);
-}
 
-
-/**
- * Открывает попап
- * @returns {void}
- */
-function closePopup() {
-  popup.classList.remove(`popup_opened`);
-}
-
-
-/**
- * Записывет в форму редактирования текущие значения имени и описания профиля
- * @returns {void}
- */
-function getProfileData() {
   inputProfileName.value = profileName.textContent;
   inputProfileDesc.value = profileDescription.textContent;
 }
 
 
-/**
- * Меняет значения полей профиля на значения, полученные из формы редактирования
- * @returns {void}
- */
-function changeProfileData() {
+//Закрывает попап
+function closePopup() {
+  popup.classList.remove(`popup_opened`);
+}
+
+
+//Сохраняет введённые данные
+function saveProfileData(evt) {
+  evt.preventDefault();
+
   profileName.textContent = inputProfileName.value;
   profileDescription.textContent = inputProfileDesc.value;
-}
 
-
-/**
- * Обработчик клика на кнопке "редактировать профиль"
- * @param {event} evt click
- * @returns {void}
- */
-function editProfileClickHandler(evt) {
-  evt.preventDefault();
-  openPopup();
-  getProfileData();
-}
-
-
-/**
- * Обработчик "отправки" формы
- * @param {event} evt submit
- * @returns {void}
- */
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  changeProfileData();
   closePopup();
 }
 
 
-editButton.addEventListener(`click`, editProfileClickHandler);
+editButton.addEventListener(`click`, openPopup);
 closeButton.addEventListener(`click`, closePopup);
-formEditProfile.addEventListener(`submit`, formSubmitHandler);
+formEditProfile.addEventListener(`submit`, saveProfileData);
